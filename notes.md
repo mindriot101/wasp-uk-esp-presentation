4aae8a90ef7f56043a3b09831efb597e32035a08
Simon Walker <s.r.walker101@googlemail.com>
2015-03-05 17:51:57 +0000

    Initial commit

---
+++ notes.md
@@ -0,0 +1,21 @@
+# Outline
+
+* Introduction - background
+  * dominant migration method uncertain
+  * imprints evidence in orbital period distribution (pile up)
+  * jupiters dominant migration force
+  * most jupiters come from WASP
+  * heavily affected by selection effects
+
+* Computing sensitivity map
+  * Choose stellar and planet sample to maximise efficiency
+  * Simulate transit signals
+  * Run WASP pipeline and track recovery rate
+
+* Casting underlying distribution
+  * Invert observed planets through sensitivity map
+  * Peak found at ~4 days
+
+* Consequences...
+  * Roche limit consistent with Ford & Rasio
+  * Pile up - characteristic to hot Jupiters
---
+++ scripts/plot_sensitivity_map.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+from mpl_toolkits.axes_grid1 import AxesGrid
+import numpy as np
+import os
+from planetdatabase.waspdatabase import SouthernPublishedDatabase
+import astropy.constants as const
+import tables
+
+import seaborn as sns
+
+sns.set(style='white', context='talk')
+
+secondsInDay = 86400.
+GravConst = const.G.value
+
+def roche_radius(period, mp):
+    '''
+    Given the period and planetary mass, calculate the radius value
+    '''
+    period = period * secondsInDay
+    mp = mp * const.M_jup.value
+
+    C = 0.46224
+    result = C * (((GravConst * mp * period ** 2) / (4. * np.pi ** 2))
+            ** (1. / 3.))
+    return result / const.R_jup.value
+
+def set_axis_limits(ax):
+    ticks = [0.1, 0.2, 0.5, 1, 2, 5, 10]
+
+    period_limits = (0.35, 10)
+    radius_limits = (0.3, 6)
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+
+    ax.set_xticks(ticks)
+    ax.set_xticklabels(ticks)
+
+    ax.set_yticks(ticks)
+    ax.set_yticklabels(ticks)
+
+    ax.set_xlim(*period_limits)
+    ax.set_ylim(*radius_limits)
+
+
+def label_axis(ax):
+    ax.set_xlabel(r'Orbital period / days')
+    ax.set_ylabel(r'Planetary radius / $\mathrm{R_J}$')
+
+
+def plot_roche_limits(ax):
+    roche_periods = np.linspace(0.35, 10, 10)
+    for rp in [roche_radius(roche_periods, mp) for mp in [0.5, 2, 10]]:
+        ax.plot(roche_periods, rp, 'w--')
+
+def render_planets(axis):
+    wd = SouthernPublishedDatabase()
+    population = wd.data()
+    (_, _, period, period_up, period_low, radius, radius_up, radius_low) = map(
+        np.array, zip(*population))
+
+    axis.errorbar(period, radius, yerr=[radius_low, radius_up],
+            xerr=[period_low, period_up], ls='None', capsize=0., lw=2.,
+            marker='o', ms=5., color='w', mec='k')
+
+
+def main():
+    outfilename = os.path.join(
+            os.path.dirname(__file__),
+            '..', 'images', 'plots', 'sensitivitymap.pdf')
+    data_filename = os.path.expanduser(
+            os.path.join(
+                '~', 'work', 'WASP', 'DistributionAnalyser', 'fields.h5'))
+
+
+
+    with tables.open_file(data_filename) as infile:
+        node = infile.root.debug.oriondetectionmap
+        period = np.log10(node.period[:])
+        radius = np.log10(node.radius[:])
+        orion_detection_map = node.map[:]
+
+        node = infile.root.sensitivity.map
+        prob = node.prob[:]
+        noprob = node.noprob[:]
+
+    period_centres = 10 ** (period + np.diff(period)[0] / 2.)
+    radius_centres = 10 ** (radius + np.diff(radius)[0] / 2.)
+
+    cmap = plt.cm.afmhot
+
+    panel_width = 4
+    panel_height = 3
+
+    with PdfPages(outfilename) as pdfpages:
+        v_range = (0.0, 1.0)
+        vmin, vmax = v_range
+
+        for data in [orion_detection_map, noprob, prob]:
+            fig, axis = plt.subplots()
+            axis.pcolormesh(period_centres, radius_centres, data,
+                    vmin=vmin, vmax=vmax, cmap=cmap)
+            set_axis_limits(axis)
+            label_axis(axis)
+            plot_roche_limits(axis)
+            render_planets(axis)
+            fig.tight_layout()
+            pdfpages.savefig(fig)
+
+
+if __name__ == '__main__':
+    main()
