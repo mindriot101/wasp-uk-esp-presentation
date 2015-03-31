05426c5720285f15f4280037c401065ca420d403
Simon Walker <s.r.walker101@googlemail.com>
2015-03-31 14:47:02 +0100

    Plot the distributions more clearly

--- scripts/plot_jupiter_distributions.py
+++ scripts/plot_jupiter_distributions.py
@@ -40,14 +40,21 @@ def main(args):
     kepler_ind = df['NAME'].str.contains('Kepler')
     kepler = df[kepler_ind]
 
+    colours = sns.color_palette(n_colors=3)
+
     fig, axis = plt.subplots()
 
     lims = (0.6, 10)
     axis.hist([wasp.PER.values, rv.PER.values, kepler.PER.values],
             bins=np.logspace(np.log10(0.68399037867067902),
                 np.log10(50), 13),
-            histtype='step', label=['WASP', 'RV', 'Kepler'],
-            lw=2.)
+            histtype='stepfilled', alpha=0.2, label=['WASP', 'RV', 'Kepler'],
+            lw=2., edgecolor='k', color=colours)
+    axis.hist([wasp.PER.values, rv.PER.values, kepler.PER.values],
+            bins=np.logspace(np.log10(0.68399037867067902),
+                np.log10(50), 13),
+            histtype='step',
+            lw=2., color=colours)
     axis.set_xscale('log')
     axis.legend(loc='best')
     axis.xaxis.set_major_locator(plt.LogLocator(subs=[1, 2, 5]))
